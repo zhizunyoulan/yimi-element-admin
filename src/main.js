04e802addc1f7362b39ed3511988fecacb9106f9
uublue <uublue@126.com>
2024-04-23 22:36:57 +0800

    整理main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from "vue";
 import App from "./App";
+import Cookies from "js-cookie";
+// 引入 ElementUI
+import ElementUi from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUi, {
+  size: Cookies.get("size") || "medium", // set element-ui default size
+});
+import "@/assets/icons"; // icon
+import "@/assets/css/yimi.scss";
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
+
+// 引入YimiElement
+import YimiElement from "@uublue/yimi-element";
+import "@uublue/yimi-element/lib/yimi-element.css";
+Vue.use(YimiElement);
 
 //执行扫描页面，要先于构造router和store
 import {scanViews} from '@/utils/page'
@@ -7,38 +22,6 @@ scanViews()
 
 import router from "./router";
 import store from "./store";
-import Cookies from "js-cookie";
-
-// 引入 ElementUI
-import ElementUi from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-Vue.use(ElementUi, {
-  size: Cookies.get("size") || "medium", // set element-ui default size
-});
-
-import './assets/icons' // icon
-
-// 引入YimiElement
-import YimiElement from "@uublue/yimi-element";
-import "@uublue/yimi-element/lib/yimi-element.css";
-Vue.use(YimiElement);
-
-import "normalize.css/normalize.css"; // A modern alternative to CSS resets
-// import "@/assets/icons"; // icon
-import "@/assets/css/yimi.scss";
-
-Vue.prototype.console = {
-  log: console.log,
-  info: console.info,
-  error: console.error,
-};
-
-Vue.prototype.setTimeout = (code, milliseconds) => {
-  window.setTimeout(code, milliseconds);
-};
-Vue.prototype.setInterval = (code, milliseconds) => {
-  window.setInterval(code, milliseconds);
-};
 
 //处理接口并混入到组件中
 import { convertApis } from "@/utils/api";
@@ -53,6 +36,19 @@ Vue.mixin({
     };
   },
 });
+
+// 将console、setTimeout、setInterval等window属性给到Vue原型，方便在template中使用
+Vue.prototype.console = {
+  log: console.log,
+  info: console.info,
+  error: console.error,
+};
+Vue.prototype.setTimeout = (code, milliseconds) => {
+  window.setTimeout(code, milliseconds);
+};
+Vue.prototype.setInterval = (code, milliseconds) => {
+  window.setInterval(code, milliseconds);
+};
 
 Vue.config.productionTip = false;
 Vue.prototype.$store = store;
